9a42619739afff63a8ead6738e05ca1fe2ee898e
Keyvan Fatehi <keyvanfatehi@gmail.com>
2013-07-30 20:22:33 -0700

    Cleanup and setup bait test

--- .bait/test.sh
+++ .bait/test.sh
@@ -3,15 +3,7 @@ bait_dir=$(dirname $0)
 project_dir="$bait_dir/.."
 cd $project_dir
 
-rvm info
-
-pwd
-
-if [[ -f ./Gemfile ]]; then
-  export BUNDLE_GEMFILE=$project_dir/Gemfile
-  echo "Gemfile exists -- bundling"
-  bundle install
-  bundle exec rake spec
-else
-  rake spec
-fi
+export BUNDLE_GEMFILE=$project_dir/Gemfile
+echo "bundling"
+bundle install > /dev/null 2>&1
+motion-specwrap
--- Rakefile
+++ Rakefile
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 $:.unshift("/Library/RubyMotion/lib")
 require 'motion/project/template/ios'
-require 'bundler'
-Bundler.require
+# require 'bundler'
+# Bundler.require
+
+
 
 Motion::Project::App.setup do |app|
   # Use `rake config' to see complete project settings.
   app.name = 'baitmotion'
-  app.redgreen_style = :full
+  #app.redgreen_style = :full
 end
