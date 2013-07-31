d125954f5043ea30937f4795631d10505a6bc05b
Keyvan Fatehi <keyvanfatehi@gmail.com>
2013-07-30 18:16:52 -0700

    update script

--- .bait/test.sh
+++ .bait/test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash --login
 bait_dir=$(dirname $0)
 project_dir="$bait_dir/.."
 cd $project_dir
@@ -6,6 +6,7 @@ cd $project_dir
 if [[ -f ./Gemfile ]]; then
   echo "Gemfile exists -- bundling"
   bundle
+  bundle exec rake spec
+else
+  rake spec
 fi
-
-rake spec
